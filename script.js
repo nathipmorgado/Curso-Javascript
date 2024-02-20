84b752a07c7f4a9dfd867510bd491fb54c9f5018
nathipmorgado <nathaliapmorgado@gmail.com>
2024-02-20 15:17:46 -0300

    Tipagem ok

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+let nome = 'Nathália';
 let idade = 27;
-console.log(idade);
-
-let nome = "Nathália";
-console.log(nome);
-
-let profissao = "Programadora";
-console.log(profissao);
-
-const valorIngressoAdulto = 20;
-console.log(valorIngressoAdulto);
+let estaAprovado = true;
+let sobrenome = undefined;
+let filhos = true;
+quantidadeFilhos = 1;
 
 
+
+//f12 - console - typeof + nome da variavel = tipo da variavel
+//ex: typeof nome + enter = 'string'
+//"nome = 5
+//5
+//typeof nome
+//'number' - pode ser alterado a qualquer momento
+
+let pessoa = {
+    nome:'Nathália',
+    idade: 27,
+    estaAprovado: true,
+    sobrenome: 'Peres',
+    filhos: true,
+    quantidadeFilhos: 1
+};
+console.log(pessoa);
